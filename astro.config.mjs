df6017caa85cbf64a15bcebd87e43fec89674396
thijsBoet <m.boet@chello.nl>
2023-04-16 15:31:08 +0200

    finished homepage starting dynamic classes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,3 +2,11 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({});
+
+// React integration
+// import react from "@astrojs/react";
+
+// // https://astro.build/config
+// export default defineConfig({
+//   integrations: [react()]
+// });
